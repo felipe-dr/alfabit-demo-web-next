7a0e01331877b5380fc1f13fa90e6356a1864646
Felipe DR <felipe.corp7@gmail.com>
2023-12-29 10:33:42 -0300

    feat: class-16 consuming design system

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Button, Typograph, Link, Input } from "alfabit-ds/components";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-5">
+      <div className="px-8 py-5 rounded-md flex flex-col gap-3 text-center text-gray-950 bg-gray-800">
+        <Typograph element="h3" size="title3" className="font-black text-white">
+          Login
+        </Typograph>
+        <Input label="Email" className="outline-none w-96" />
+        <Input label="Senha" className="outline-none w-96" />
+        <Button variant="primary">Clique aqui</Button>
+        <Link href="#">Esqueceu sua senha? Clique aqui!</Link>
+      </div>
+    </main>
+  );
+}
